741d355a7035115b9e1b9095fbdc8835d7696a5d
Alex Korotkov <durm.icecoffee@gmail.com>
2015-09-21 00:10:39 +0300

    count

--- generate_data.py
+++ generate_data.py
@@ -16,8 +16,14 @@ def generate_ip():
 
 if __name__ == "__main__":
     from giant.db import conn, sql_generate_iptable_item
+    import sys
     
+    try:
+        c = int(sys.argv[1])
+    except:
+        c = 1000
+        
     cur = conn.cursor()
     
-    for i in range(10000000):
+    for i in range(c):
         cur.execute(sql_generate_iptable_item(generate_user_id(), generate_ip(), generate_datetime()))
